d2fa02a87b92ad7ceb4e3098ce863a41de50a76c
Nishar Khorajiya <110724795+nishar-khorajiya@users.noreply.github.com>
2024-02-13 22:37:24 +0530

    Create 1572. Matrix Diagonal Sum.cpp

---
+++ 1572. Matrix Diagonal Sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int sum=0,n=mat.size();
+
+        for(int i=0;i<n;i++){
+            sum+=mat[i][i];
+            sum+=mat[i][n-i-1];
+        }
+
+        if(n%2==1){
+            sum-=mat[n/2][n/2];
+        }
+
+        return sum;
+    }
+};
